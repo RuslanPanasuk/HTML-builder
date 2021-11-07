5b880c38f796fb45d44931e3889128b169a1d717
RuslanPanasuk <panasuk.eetp@gmail.com>
2021-11-08 00:06:52 +0200

    fix: add another desigion 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,37 @@
 const path = require ('path');
 const fs = require ('fs');
 
-fs.readFile(
+
+//fs.ReadStream наследует от stream.Readable
+var stream = new fs.ReadStream(
     path.join(__dirname, 'text.txt'),
-    'utf-8',
-    (err, data) => {
-        if (err) throw err;
-        console.log(data);
+    'utf-8'
+);
+ 
+stream.on('readable', function(){
+    var data = stream.read();
+    if (data != null);
+    console.log(data);
+});
+ 
+stream.on('end', function(){
+    console.log("THE END");
+});
+
+stream.on('error', function(err){
+    if(err.code == 'ENOENT'){
+        console.log("Файл не найден");
+    }else{
+        console.error(err);
     }
-)
+});
+
+// Вариант который не соответствует заданию
+// fs.readFile(
+//     path.join(__dirname, 'text.txt'),
+//     'utf-8',
+//     (err, data) => {
+//         if (err) throw err;
+//         console.log(data);
+//     }
+// )
